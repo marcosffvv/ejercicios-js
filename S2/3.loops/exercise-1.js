f694cb0f0cdadc6151f0f11770d9eae09be81cad
marcosffvv <marcosfrailefdz02@gmail.com>
2022-12-13 19:03:06 +0100

    recuperacion

--- S2/3.loops/exercise-1.js
+++ S2/3.loops/exercise-1.js
@@ -2,3 +2,7 @@
 
 const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
 
+for (let places of placesToTravel) {
+    console.log(places);
+}
+
--- S2/3.loops/exercise-2.js
+++ S2/3.loops/exercise-2.js
@@ -1,8 +1,12 @@
-//Usa un foin para imprimir por consola los datos del alienigena.
+//Usa un for in para imprimir por consola los datos del alienigena.
 
 const alien = {
     name: 'Wormuck',
     race: 'Cucusumusu',
     planet: 'Eden',
     weight: '259kg'
+}
+
+for (let property in alien) {
+    console.log(alien[property]);
 }
